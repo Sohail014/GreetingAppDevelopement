ff63e8b3ed21536d9683f5c86b1808f265786d14
sohail <sohailss2010@gmail.com>
2022-02-27 20:21:02 +0530

    first commit

---
+++ src/main/java/com/bridgelabz/greetingappdevelement/GreetingappdevelementApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.greetingappdevelement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingappdevelementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingappdevelementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/greetingappdevelement/GreetingappdevelementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.greetingappdevelement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingappdevelementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
